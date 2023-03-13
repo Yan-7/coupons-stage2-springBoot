b0fd63727a8897900c3f6030b8f680fc0141b686
Yan <yanivalmagor@gmail.com>
2023-03-13 21:26:03 +0200

    13.3

--- src/main/java/com/couponsstage2/dailyJob/CouponExpiration.java
+++ src/main/java/com/couponsstage2/dailyJob/CouponExpiration.java
@@ -1,6 +1,28 @@
 package com.couponsstage2.dailyJob;
 
-// TODO: 14/02/2023  
-public class CouponExpiration extends Thread{
+import com.couponsstage2.enteties.Coupon;
+import com.couponsstage2.repositories.CouponsRep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+@Component
+public class CouponExpiration{
+
+    @Autowired
+    CouponsRep couponsRep;
+
+    // TODO: 13/03/2023 we get the coupons with the repository
+    @Scheduled (timeUnit = TimeUnit.SECONDS,fixedRate = 5)
+    public void deleteCoupons(List<Coupon> couponList) {
+        for (Coupon c: couponList) {
+            if (c.getEndDate().isAfter(LocalDate.now())) {
+                couponsRep.deleteById(c.getId());
+            }
+        }
+    }
 
 }
